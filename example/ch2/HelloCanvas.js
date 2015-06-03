1c26fece920863b10d29be62bc0aebb23fb3a294
guagua <aarongua@163.com>
2015-06-03 12:28:08 +0800

    修改格式

--- example/ch2/HelloCanvas.js
+++ example/ch2/HelloCanvas.js
@@ -15,12 +15,10 @@ function draw() {
     context.fillStyle="#FF0000";//设置填充颜色
     context.fillRect(10,0,150,100);//绘制矩形
 
-    context.beginPath();
     context.strokeStyle="#00FF00";//设置边框
     context.lineWidth=2;//边框宽度
     context.strokeRect(10,150,150,100);
 
-    context.beginPath();
     context.moveTo(10,300);
     context.lineTo(150,300);
     context.lineTo(80,400);
